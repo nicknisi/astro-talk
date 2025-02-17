81b22e7c9115bd9ab6bd84ea697e3db846f98f42
Nick Nisi <nick@nisi.org>
2025-02-17 11:28:10 -0600

    initial setup of slides framework

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const slides = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      type: z.enum(["title", "content"]),
+      order: z.number().optional(),
+      image: image().optional(),
+    }),
+});
+
+export const collections = { slides };
---
+++ src/content/slides/002-intro.md
@@ -0,0 +1,16 @@
+---
+title: "Overview"
+type: "content"
+order: 2
+---
+
+Here's some **markdown** content with some code:
+
+
+```ts
+function helloWorld() {
+    console.log('Hello, World');
+}
+```
+
+
