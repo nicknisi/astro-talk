f1994c4af822a50080f68321513d4a017f7b85ac
Nick Nisi <nick@nisi.org>
2025-02-25 23:44:03 -0600

    add endpoint demo

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,45 @@
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import icon from "astro-icon";
-
-import react from "@astrojs/react";
-
-import svelte from "@astrojs/svelte";
-
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import { readFileSync } from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), rawBuffer()],
   },
 
   integrations: [icon(), react(), svelte(), mdx()],
   markdown: {
     shikiConfig: {
-      theme: "dracula",
+      theme: 'dracula',
       wrap: true,
     },
   },
 });
+
+/**
+ * Vite plugin to load raw buffer data from a file.
+ * This is useful for loading binary data like images or fonts.
+ * @returns a Vite plugin object
+ * @see https://vitejs.dev/config/#plugins
+ */
+function rawBuffer() {
+  return {
+    name: 'vite-plugin-raw-buffer',
+    transform(_: unknown, id: string) {
+      if (id.endsWith('?buffer')) {
+        const buffer = readFileSync(id.replace(/\?buffer$/, ''));
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+      return undefined;
+    },
+  };
+}
+
---
+++ src/pages/endpoint.png.ts
@@ -0,0 +1,114 @@
+import satori from 'satori';
+import sharp from 'sharp';
+import type { APIRoute } from 'astro';
+import { type ReactNode } from 'react';
+import markedSafeImage from '@/assets/marked-safe.png?buffer';
+import dmSansBoldData from '../../public/DMSans-Bold.ttf?buffer';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ url }) => {
+  const params = url.searchParams;
+  const title = params.get('title') ?? 'Next.js';
+  console.log('title', title, params, url);
+
+  const markedSafe = `data:image/png;base64,${Buffer.from(markedSafeImage).toString('base64')}`;
+
+  let background: Record<string, unknown> = {
+    backgroundImage: 'linear-gradient(to bottom, #11181C, #0b1215)',
+    backgroundColor: '#0b1215',
+  };
+
+  const width = 618;
+  const height = 499;
+
+  const node: ReactNode = {
+    type: 'div',
+    props: {
+      children: [
+        {
+          type: 'div',
+          props: {
+            children: [
+              {
+                type: 'div',
+                props: {
+                  children: '',
+                  style: {
+                    backgroundImage: `url('${markedSafe}')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: '#Ob1215',
+                    backgroundSize: '100% 100%',
+                    width: `${width}px`,
+                    height: `${height}px`,
+                    position: 'relative',
+                  },
+                },
+              },
+              {
+                type: 'div',
+                props: {
+                  children: [
+                    {
+                      type: 'h1',
+                      props: {
+                        children: title,
+                        style: {
+                          fontFamily: 'DM Sans Bold',
+                          position: 'absolute',
+                          fontSize: 60,
+                          color: '#000000',
+                        },
+                      },
+                    },
+                  ],
+                  style: {
+                    display: 'flex',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '35%',
+                    textAlign: 'center',
+                  },
+                },
+              },
+            ],
+            style: {
+              display: 'flex',
+              alignItems: 'center',
+              gap: '1rem',
+              padding: '1rem',
+              flexGrow: 1,
+            },
+          },
+        },
+      ],
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        ...background,
+      },
+    },
+  } as ReactNode;
+
+  const svg = await satori(node, {
+    width,
+    height,
+    fonts: [
+      {
+        data: Buffer.from(dmSansBoldData),
+        name: 'DM Sans Bold',
+        style: 'normal',
+      },
+    ],
+  });
+
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+  return new Response(png, {
+    headers: {
+      'Content-Type': 'image/png',
+    },
+  });
+};
